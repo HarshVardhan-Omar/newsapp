36ad2eedadc4548c7ae4a87ae2b31a9a64dbe2d0
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2021-12-29 01:35:05 +0530

    Enter

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -13,49 +14,61 @@ export class News extends Component {
   //firstly constructor will run then render and at the last componentDidMount
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=a165f78855854571bfd8e546e87441bc&page=1&pageSize=20";
-    let data = await fetch(url);
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=a165f78855854571bfd8e546e87441bc&page=1&pageSize=${this.props.pageSize}`;
+      this.setState({loading: true});
+      let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles ,totalResults:parsedData.totalResults});
+    this.setState({ articles: parsedData.articles ,
+      totalResults:parsedData.totalResults,
+    loading:false});
   }
    handlePrevious = async() => {
     let url =
-    `https://newsapi.org/v2/top-headlines?country=in&apiKey=a165f78855854571bfd8e546e87441bc&page=${this.state.page - 1}&pageSize=20`;
-  let data = await fetch(url);
+    `https://newsapi.org/v2/top-headlines?country=in&apiKey=a165f78855854571bfd8e546e87441bc&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
+  
+    let data = await fetch(url);
   let parsedData = await data.json();
   console.log(parsedData);
   this.setState({
       page: this.state.page - 1,
-      articles: parsedData.articles 
+      articles: parsedData.articles,
+      loading: false
     })
 
 
   }
   handleNext =  async() => {
-    if(this.state.page + 1 >Math.ceil(this.state.totalResults/20)){
+    if(!(this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize))){
+
+      let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=a165f78855854571bfd8e546e87441bc&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading: true});
+      let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false
+      })
+
+
 
     }
-    else{
-    let url =
-    `https://newsapi.org/v2/top-headlines?country=in&apiKey=a165f78855854571bfd8e546e87441bc&page=${this.state.page + 1}&pageSize=20`;
-  let data = await fetch(url);
-  let parsedData = await data.json();
-  console.log(parsedData);
-  this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles 
-    })
-  }
+   
 
   }
   render() {
     return (
       <div className="container my-3 ">
-        <h1>NewsMonkey-Top Headlines</h1>
+        <h1 className="text-center">NewsMonkey-Top Headlines</h1>
+       { this.state.loading && <Spinner/>}
 
-        <div className="row">
-          {this.state.articles.map((element) => {
+<div className="row">
+       {!this.state.loading &&  
+          this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
@@ -77,7 +90,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between ">
           <button disabled={this.state.page <=1} type="button" className="btn btn-dark" onClick={this.handlePrevious}> &larr;  Previous</button>
-          <button type="button" className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
+          <button disabled={this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
           </div>
       </div>
     );
